3fa85bec3bc1aa94c09bb73931574afc5d294cbd
CanardConfit <canardconfit.developpement@gmail.com>
2024-07-31 23:40:04 +0200

    Update

---
+++ AnnotationWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Net.Mime;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Brushes = System.Windows.Media.Brushes;
+
+namespace AstroNotation
+{
+    public partial class AnnotationWindow : Window
+    {
+        private Image _bitmapImage;
+
+        public AnnotationWindow(Image bitmap)
+        {
+            InitializeComponent();
+            BitmapImage = bitmap;
+        }
+
+        public Image BitmapImage
+        {
+            get => _bitmapImage;
+            set => _bitmapImage = value;
+        }
+
+        private void AddAnnotationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(AnnotationTextBox.Text)) return;
+            
+            using (Graphics g = Graphics.FromImage(BitmapImage))
+            {
+                g.DrawString(AnnotationTextBox.Text, new Font("Arial", 20), System.Drawing.Brushes.Black, new PointF(10, 10));
+            }
+            DialogResult = true;
+            Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,68 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace AstroNotation;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace AstroNotation
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private Image _image;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpeg;*.jpg)|*.jpeg;*.jpg";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _image = Image.FromFile(openFileDialog.FileName);
+                ImageCanvas.Children.Clear();
+                ImageCanvas.Children.Add(new System.Windows.Controls.Image() {Source = ConvertDrawingImageToImageSource(_image), Width = _image.Width, Height = _image.Height});
+            }
+        }
+
+        private void SaveImageMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JPEG files (*.jpeg)|*.jpeg";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+        private void AddAnnotationMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AnnotationWindow annotationWindow = new AnnotationWindow(_image);
+            annotationWindow.ShowDialog();
+        }
+        
+        public static ImageSource ConvertDrawingImageToImageSource(System.Drawing.Image image)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Save the System.Drawing.Image to the memory stream
+                image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+            
+                // Reset the stream position to the beginning
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                // Create a BitmapImage and begin its initialization
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
     }
 }
